b98bb1027fa98e4f1cb9f52f6b7206b99797c213
小狼 <whitefang0207@gmail.com>
2026-05-14 13:59:49 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'wolf-tasks-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
